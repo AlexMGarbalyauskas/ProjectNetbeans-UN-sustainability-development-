1c6a3f8fe0360498e52fd663f2f4051a1a586d25
NCIAlexGar <116267528+NCIAlexGar@users.noreply.github.com>
2023-11-26 17:46:08 +0000

    Add files via upload

---
+++ ForestApp1/src/forestapp1/ForestApp1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package forestapp1;
+
+/**
+ *
+ * @author alexb
+ */
+public class ForestApp1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        ForestGUI myGUI = new ForestGUI();
+        myGUI.setVisible(true);
+    }
+    
+}
