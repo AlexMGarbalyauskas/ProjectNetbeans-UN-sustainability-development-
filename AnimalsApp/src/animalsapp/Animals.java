12bdf269897363f4c7c4f3fbb9711b88a9ba0494
NCIAlexGar <116267528+NCIAlexGar@users.noreply.github.com>
2023-11-26 17:45:09 +0000

    Add files via upload

---
+++ AnimalsApp/src/animalsapp/Animals.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package animalsapp;
+/**
+ *
+ * @author Alex
+ */
+public abstract class Animals {
+    
+    String animalName; 
+    String species; 
+    String habitat;
+    String location; 
+    String status; 
+    int id; 
+    int population; 
+    
+    public Animals(String animalName,String species, String habitat,String location, String status, int population, int id){
+        this.animalName = animalName;
+        this.species = species; 
+        this.habitat = habitat;
+        this.location = location; 
+        this.status = status; 
+        this.population = population; 
+        this.id = id; 
+        
+    }
+    
+    
+    public abstract double Charity();
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+    
+   
+}
