1e9fa857b08433615ae57dfee610a2305ae65fb6
NCIAlexGar <116267528+NCIAlexGar@users.noreply.github.com>
2023-12-04 14:40:45 +0000

    Add files via upload

---
+++ AnimalsApp/src/animalsapp/SnowLeopard.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package animalsapp;
+
+/**
+ *
+ * @author Alex
+ */
+public class SnowLeopard extends Animals{
+
+    public SnowLeopard(String animalName, String species, String habitat, String location, String status, int population, int id) {
+        super(animalName, species, habitat, location, status, population, id);
+    }
+
+    @Override
+    public double Charity() {
+       
+        return 0;
+       
+    }
+}
