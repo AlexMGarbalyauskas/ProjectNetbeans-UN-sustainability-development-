ea2369783262c7630adf1ee131ee7e49c2c6dae3
NCIAlexGar <116267528+NCIAlexGar@users.noreply.github.com>
2023-12-11 10:50:20 +0000

    Add files via upload

---
+++ AnimalsApp/src/animalsapp/AnimalsApp.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package animalsapp;
+
+
+public class AnimalsApp {
+public static void main(String[] args) {
+        //displays the Menu JFrame
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                Menu frame = new Menu();
+                frame.setVisible(true);
+            }
+        });
+    
+      
+    }
+}
+
+
+
+    
+
+
+
+
+
+
